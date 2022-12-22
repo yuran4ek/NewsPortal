3fe1a20aac86ae18d0de0edf3c2aa29c5a987e9b
Yuri.Batanov <yurok_37@icloud.com>
2022-12-22 22:19:33 +0600

    update censor for posts

--- news/templatetags/custom_filter.py
+++ news/templatetags/custom_filter.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.filter()
-def censor(value):
+def censor(value: str, symbol='*'):
     bad_words = [
         'матч',
         'Челси',
@@ -28,7 +28,15 @@ def censor(value):
 
     if not isinstance(value, str):
         raise TypeError('Переменная должна быть строкового типа!')
-    for word in bad_words:
-        pattern = (r"\b" + re.escape(word) + r"\b")
-        value = re.sub(pattern, (word[0] + (len(word) - 2) * '*') + word[-1], value, flags=re.I)
+    # Проверка, есть ли плохие слова в тексте
+    badwords_in_text = set(value.lower().split()) & set(bad_words)
+    if badwords_in_text:
+        for badword in badwords_in_text:
+            # Для слов с заглавной буквы
+            badword_pattern_up = r"\b" + re.escape(badword.capitalize()) + r"\b"
+            # Для слов со строчной буквы
+            badword_pattern_low = r"\b" + re.escape(badword) + r"\b"
+            value = re.sub(badword_pattern_up, badword.capitalize()[0] + (len(badword) - 2) * symbol + badword[-1],
+                           value)
+            value = re.sub(badword_pattern_low, badword[0] + (len(badword) - 2) * symbol + badword[-1], value)
     return value
